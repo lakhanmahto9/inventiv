a94e022b9e738b1ca0913f5f3b5f3effe607e9c8
lakhan <lakhanmahto3676@gmail.com>
2026-01-26 12:30:40 +0530

    social media linked added

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import Layout from '../layout/Layout'
-import { EmailIcon, Facebook, HomeIcon, LinkedIn, PhoneIcon, Twitter } from '../../icons/Icons'
+import React from "react";
+import Layout from "../layout/Layout";
+import {
+  EmailIcon,
+  Facebook,
+  HomeIcon,
+  LinkedIn,
+  PhoneIcon,
+  Twitter,
+} from "../../icons/Icons";
 
 const Contact = () => {
   return (
-    <section id='contact'>
+    <section id="contact">
       <div
         className="min-h-auto bg-cover bg-center bg-no-repeat flex justify-center items-center bg-black/30 opacity-90"
         style={{
@@ -12,66 +19,100 @@ const Contact = () => {
         }}
       >
         <div className="w-full py-20">
-          <div className='w-full flex justify-center items-center'>
-            <p className='text-5xl text-white font-bold'>Contact Us</p>
+          <div className="w-full flex justify-center items-center">
+            <p className="text-5xl text-white font-bold">Contact Us</p>
           </div>
-          <div className='px-4 2xl:px-96 text-white  w-full mt-8 sm:mt-20'>
-            <div className='w-full flex flex-col lg:flex-row p-4 justify-around gap-8'>
-              <div className='w-full lg:w-1/2 border border-blue-600 flex items-center justify-center rounded-4xl p-4'>
-                <div className='flex flex-col gap-4 justify-center items-center'>
+          <div className="px-4 2xl:px-96 text-white  w-full mt-8 sm:mt-20">
+            <div className="w-full flex flex-col lg:flex-row p-4 justify-around gap-8">
+              <div className="w-full lg:w-1/2 border border-blue-600 flex items-center justify-center rounded-4xl p-4">
+                <div className="flex flex-col gap-4 justify-center items-center">
                   <div>
-                    <div className='flex flex-row gap-2'>
-                      <div className='bg-white w-10 h-10 rounded-full flex items-center justify-center'>
+                    <div className="flex flex-row gap-2">
+                      <div className="bg-white w-10 h-10 rounded-full flex items-center justify-center">
                         <HomeIcon color={"#000000"} width={20} height={20} />
                       </div>
                       <div>
-                        <p className='text-3xl font-bold text-[#00BCD5]'>Venue</p>
-                        <p className='lg:text-base text-sm'>IIT(ISM) Dhanbad, February 21-22, 2026</p>
-                      </div>
-                    </div>
-
-                    <div className='flex flex-col'>
-                      <div className='flex gap-2'>
-                        <div className='bg-white w-10 h-10 rounded-full flex items-center justify-center'>
-                          <PhoneIcon color={"#000000"} width={20} height={20} />
-
-                        </div>
-                        <p className='text-2xl sm:text-3xl font-bold text-[#00BCD5]'>Contact</p>
-                      </div>
-                      <div className='ml-12'>
-                        <p className='lg:text-base text-sm'>
-                          Prof. Parthasarathi Das (Dean R &D), Convenor, IInvenTiv 2026
+                        <p className="text-3xl font-bold text-[#00BCD5]">
+                          Venue
                         </p>
-                        <p className='lg:text-base text-sm'>Prof. Ejaz Ahmad (PIC SRIC) Organizing Secretary, IInvenTiv 2026</p>
+                        <p className="lg:text-base text-sm">
+                          IIT(ISM) Dhanbad, February 21-22, 2026
+                        </p>
                       </div>
                     </div>
 
-                    <div className='flex flex-row gap-2'>
-                      <div className='bg-white w-10 h-10 rounded-full flex items-center justify-center'>
+                    <div className="flex flex-col">
+                      <div className="flex gap-2">
+                        <div className="bg-white w-10 h-10 rounded-full flex items-center justify-center">
+                          <PhoneIcon color={"#000000"} width={20} height={20} />
+                        </div>
+                        <p className="text-2xl sm:text-3xl font-bold text-[#00BCD5]">
+                          Contact
+                        </p>
+                      </div>
+                      <div className="ml-12">
+                        <p className="lg:text-base text-sm">
+                          Prof. Parthasarathi Das (Dean R &D), Convenor,
+                          IInvenTiv 2026
+                        </p>
+                        <p className="lg:text-base text-sm">
+                          Prof. Ejaz Ahmad (PIC SRIC) Organizing Secretary,
+                          IInvenTiv 2026
+                        </p>
+                      </div>
+                    </div>
+
+                    <div className="flex flex-row gap-2">
+                      <div className="bg-white w-10 h-10 rounded-full flex items-center justify-center">
                         <EmailIcon color={"#000000"} width={20} height={20} />
                       </div>
                       <div>
-                        <p className='text-2xl sm:text-3xl font-bold text-[#00BCD5]'>Email</p>
-                        <p className='lg:text-base text-sm'>iinventiv2026@iitism.ac.in</p>
+                        <p className="text-2xl sm:text-3xl font-bold text-[#00BCD5]">
+                          Email
+                        </p>
+                        <p className="lg:text-base text-sm">
+                          iinventiv2026@iitism.ac.in
+                        </p>
                       </div>
                     </div>
                   </div>
-                  <div className='px-8 sm:px-20 py-4 border border-blue-500 rounded-2xl bg-blue-800/40 flex gap-8'>
-                    <div className='bg-slate-50 rounded-full p-2'><Twitter color={"#000000"} width={20} height={20} /></div>
-                    <div className='bg-slate-50 rounded-full p-2'><Facebook color={"#1A73E8"} width={20} height={20} /></div>
-                    <div className='bg-slate-50 rounded-full p-2'><LinkedIn color={"#1A73E8"} width={20} height={20} /></div>
+                  <div className="px-8 sm:px-20 py-4 border border-blue-500 rounded-2xl bg-blue-800/40 flex gap-8">
+                    <a
+                      href="https://x.com/iinventiv2026"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="bg-slate-50 rounded-full p-2 cursor-pointer hover:scale-110 transition"
+                    >
+                      <Twitter color={"#000000"} width={20} height={20} />
+                    </a>
+                    <a
+                      href="https://www.facebook.com/share/1HAAqhXaZa/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="bg-slate-50 rounded-full p-2 cursor-pointer hover:scale-110 transition"
+                    >
+                      <Facebook color={"#1A73E8"} width={20} height={20} />
+                    </a>
+                    <a
+                      href="https://www.linkedin.com/company/iinventiv-2026/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="bg-slate-50 rounded-full p-2 cursor-pointer hover:scale-110 transition"
+                    >
+                      <LinkedIn color={"#1A73E8"} width={20} height={20} />
+                    </a>
                   </div>
                 </div>
               </div>
-              <div className='w-full lg:w-1/2 border border-blue-600 rounded-4xl opacity-90'>
-                <img src="/contactus_card.gif" alt="" className='rounded-4xl' />
+              <div className="w-full lg:w-1/2 border border-blue-600 rounded-4xl opacity-90">
+                <img src="/contactus_card.gif" alt="" className="rounded-4xl" />
               </div>
             </div>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,12 @@
-
-
 import React from "react";
 import { Link } from "react-router-dom";
-import { CalenderIcon, Facebook, LinkedIn, LocationIcon, Twitter } from "../../icons/Icons";
+import {
+  CalenderIcon,
+  Facebook,
+  LinkedIn,
+  LocationIcon,
+  Twitter,
+} from "../../icons/Icons";
 import { motion } from "framer-motion";
 
 const footerParent = {
@@ -39,8 +43,10 @@ const Footer = () => {
       viewport={{ amount: 0.4 }}
     >
       {/* 🔥 Gradient Top Border */}
-      <div className="absolute top-0 left-0 w-full h-px 
-      bg-linear-to-r from-blue-500 via-[#ffa343] to-red-500"></div>
+      <div
+        className="absolute top-0 left-0 w-full h-px 
+      bg-linear-to-r from-blue-500 via-[#ffa343] to-red-500"
+      ></div>
 
       {/* Logo */}
       <div className="flex flex-col sm:flex-row gap-8 sm:gap-16">
@@ -53,16 +59,22 @@ const Footer = () => {
           </div>
         </motion.div>
 
-
-        <motion.div className="bg-white rounded-2xl p-8 my-4 flex flex-col justify-center items-center" variants={footerItem}>
+        <motion.div
+          className="bg-white rounded-2xl p-8 my-4 flex flex-col justify-center items-center"
+          variants={footerItem}
+        >
           <motion.div className="flex gap-2 items-center">
             <LocationIcon color="#9E0024" width="18" height="18" />
             <p className="text-[#9E0024] text-xl font-semibold">Venue</p>
           </motion.div>
-          <p className="ml-6 text-xl font-semibold text-blue-900">IIT(ISM) Dhanbad</p>
+          <p className="ml-6 text-xl font-semibold text-blue-900">
+            IIT(ISM) Dhanbad
+          </p>
           <motion.div className="flex gap-2 items-center">
             <CalenderIcon color="#9E0024" width="18" height="18" />
-            <p className="text-[#9E0024] text-xl font-semibold">February 21-22, 2026</p>
+            <p className="text-[#9E0024] text-xl font-semibold">
+              February 21-22, 2026
+            </p>
           </motion.div>
         </motion.div>
       </div>
@@ -70,7 +82,9 @@ const Footer = () => {
       {/* Column 1 */}
       <div className="flex flex-col sm:flex-row gap-4 sm:gap-16">
         <motion.div className="flex flex-col my-4" variants={footerItem}>
-          <p className="text-2xl text-[#ffa343] font-semibold">Important Links</p>
+          <p className="text-2xl text-[#ffa343] font-semibold">
+            Important Links
+          </p>
           <div className="text-white text-sm lg:text-lg">Home</div>
           <div className="text-white text-sm lg:text-lg">R&D Innovations</div>
           <div className="text-white text-sm lg:text-lg">Contact</div>
@@ -80,16 +94,79 @@ const Footer = () => {
         <motion.div className="flex flex-col gap-2 my-4" variants={footerItem}>
           <p className="text-2xl text-[#ffa343] font-semibold">Social Media</p>
           <div className="flex gap-4">
-            <motion.div variants={iconHover} whileHover="hover">
-              <Twitter color="#fff" width="20" height="20" />
+            <motion.div
+              variants={iconHover}
+              whileHover="hover"
+              className="flex items-center justify-center"
+            >
+              <a
+                href="https://x.com/iinventiv2026"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="
+      w-10 h-10 sm:w-11 sm:h-11
+      flex items-center justify-center
+      bg-white rounded-full
+      shadow-md
+      border border-blue-200
+      cursor-pointer
+      transition-all duration-300
+      hover:shadow-lg hover:scale-110
+      active:scale-95
+    "
+              >
+                <Twitter color="#000000" width={20} height={20} />
+              </a>
             </motion.div>
 
-            <motion.div variants={iconHover} whileHover="hover">
-              <Facebook color="#fff" width="20" height="20" />
+            <motion.div
+              variants={iconHover}
+              whileHover="hover"
+              className="flex items-center justify-center"
+            >
+              <a
+                href="https://www.facebook.com/share/1HAAqhXaZa/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="
+      w-10 h-10 sm:w-11 sm:h-11
+      flex items-center justify-center
+      bg-white rounded-full
+      shadow-md
+      border border-blue-200
+      cursor-pointer
+      transition-all duration-300
+      hover:shadow-lg hover:scale-110
+      active:scale-95
+    "
+              >
+                <Facebook color="#000000" width={20} height={20} />
+              </a>
             </motion.div>
 
-            <motion.div variants={iconHover} whileHover="hover">
-              <LinkedIn color="#fff" width="20" height="20" />
+            <motion.div
+              variants={iconHover}
+              whileHover="hover"
+              className="flex items-center justify-center"
+            >
+              <a
+                href="https://www.linkedin.com/company/iinventiv-2026/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="
+      w-10 h-10 sm:w-11 sm:h-11
+      flex items-center justify-center
+      bg-white rounded-full
+      shadow-md
+      border border-blue-200
+      cursor-pointer
+      transition-all duration-300
+      hover:shadow-lg hover:scale-110
+      active:scale-95
+    "
+              >
+                <LinkedIn color="#000000" width={20} height={20} />
+              </a>
             </motion.div>
           </div>
         </motion.div>
@@ -99,4 +176,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
