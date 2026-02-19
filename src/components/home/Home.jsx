689a6ec8f2ff6d41383cb64df267ba365963e066
Lakhan Mahto <lakhanmahto3676@gmail.com>
2026-02-19 12:59:23 +0530

    guest image removed

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -233,13 +233,13 @@ const Home = () => {
                   </div>
 
                   <div className="bg-white shadow-xl rounded-2xl overflow-hidden md:flex items-center">
-                    <div className="md:w-1/3 w-full p-4">
+                    {/* <div className="md:w-1/3 w-full p-4">
                       <img
                         src="/d-pradhan.jpg"
                         alt="Shree Dharmendra Pradhan Ji"
                         className="w-full h-full rounded-2xl object-cover"
                       />
-                    </div>
+                    </div> */}
                     <div className="md:w-2/3 w-full p-8 space-y-4">
                       <h3 className="text-2xl md:text-3xl font-semibold text-indigo-700">
                         Shri Dharmendra Pradhan
@@ -251,14 +251,14 @@ const Home = () => {
                         Government of India
                       </p>
 
-                      <p className="text-gray-600 leading-relaxed">
+                      {/* <p className="text-gray-600 leading-relaxed">
                         We are privileged to welcome Shri Dharmendra Pradhan Ji,
                         Hon’ble Minister of Education, Government of India, as
                         the Chief Guest for our esteemed event. His leadership
                         and vision for advancing education, innovation, and
                         skill development continue to inspire millions across
                         the nation.
-                      </p>
+                      </p> */}
 
                       <div className="pt-4">
                         <span className="inline-block bg-indigo-100 text-indigo-700 px-4 py-2 rounded-full text-sm font-medium">
